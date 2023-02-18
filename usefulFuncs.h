c925c1af64ead63ed68091e790642e15c6215092
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-02-18 17:02:53 +0330

    Created a header file to contain useful functions

---
+++ usefulFuncs.h
@@ -0,0 +1,16 @@
+//
+// Created by Erfan on 2/18/2023.
+//
+
+#ifndef THE_C_PROGRAMMING_LANGUAGE_USEFULFUNCS_H
+#define THE_C_PROGRAMMING_LANGUAGE_USEFULFUNCS_H
+
+#endif //THE_C_PROGRAMMING_LANGUAGE_USEFULFUNCS_H
+
+int getASCIICode(char character);
+
+int getASCIICode(char character)
+{
+    printf("%d\n", character);
+}
+
