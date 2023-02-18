bc2e8ee69accae70502748a9bf49c9a98358c544
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-02-18 18:56:55 +0330

    Created 1-12

---
+++ 1-12.c
@@ -0,0 +1,29 @@
+// this is a program that outputs its input text one word per line
+
+#include "stdio.h"
+
+#define IN 1
+#define OUT 0
+
+int main()
+{
+
+    int c;
+    int state = OUT;
+
+    while ((c = getchar()) != EOF)
+    {
+
+        if (c != 32)
+        {
+            state = IN;
+            putchar(c);
+        }
+        else
+            state = OUT;
+
+        if (state == OUT)
+            printf("\n");
+    }
+
+}
