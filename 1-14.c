5ffe3075b9ab2deaac838fa743221f4d485781d1
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-02-19 23:41:39 +0330

    Created 1-14

---
+++ 1-14.c
@@ -0,0 +1,49 @@
+#include "stdio.h"
+#include "usefulFuncs.h"
+
+#define EOF '\n'
+#define CHARACTER_MAX_COUNT ('z' - 'a' + 3)
+#define SPACE_CHARACTER_INDEX 27
+#define TAB_CHARACTER_INDEX 26
+
+int main()
+{
+    int c;
+    int characterCount[CHARACTER_MAX_COUNT];
+
+    initArray(characterCount, CHARACTER_MAX_COUNT);
+
+    while ((c = getchar()) != EOF)
+    {
+        int characterIndex;
+
+        if (c == ' ')
+            characterIndex = SPACE_CHARACTER_INDEX;
+        if (c == '\t')
+            characterIndex = TAB_CHARACTER_INDEX;
+        else
+            characterIndex = 25 - ('z' - c);
+
+        characterCount[characterIndex]++;
+    }
+
+
+    for (int i = 0; i < CHARACTER_MAX_COUNT; ++i)
+    {
+        if (characterCount[i] > 0)
+        {
+            int characterASCIICode = i + 'a';
+
+            printf("%2c\tcount:%d\t\t-> ", characterASCIICode, characterCount[i]);
+
+            for (int j = 0; j < characterCount[i]; ++j)
+            {
+                printf("%c",254);
+            }
+
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
