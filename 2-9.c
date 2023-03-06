09615aae9174c7536211e4ee3902a78167ef6d1f
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-03-06 12:19:31 +0330

    Updated 2-9 (not using scanf anymore)

--- 2-9.c
+++ 2-9.c
@@ -4,9 +4,7 @@
 int main()
 {
     int input;
-    while (1)
-    {
-        scanf("%d",&input);
-        printf("%3d\n", bitcount(input));
-    }
+    while ((input = (getchar() - '0')) != EOF)
+        if (input != ('\n' - '0'))
+            printf("%3d\n", bitcount(input));
 }
