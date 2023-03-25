952a4296a985df4278fa68a8fbda054073d3ffbf
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-03-26 00:59:59 +0430

    Created 4-5

--- main2.c
+++ main2.c
@@ -2,12 +2,13 @@
 #include "stdlib.h"
 #include "ctype.h"
 #include "math.h"
-#include "usefulFuncs.h"
+#include "string.h"
 
 #define MAX_OPERATION_SIZE 100   /* Max size of the operand or operator */
 #define NUMBER '0'  /* signal that a number was found */
 #define BUFFER_SIZE 100
 #define MAX_DEPTH_OF_STACK 100
+#define COMPLEX_MATH 'e'
 
 int getch(void);
 void ungetch(int);
@@ -18,6 +19,9 @@ int nextFreePositionInBuffer = 0;
 int getop(char []);
 void push(double);
 double pop(void);
+void clearStack();
+void reverseLastToItemsInStack();
+double complexMath(char []);
 
 //  4-3, 4-4, 4-5, 4-6 (Reverse Polish Calculator)
 
@@ -25,7 +29,7 @@ double pop(void);
 int main()
 {
     int type;
-    double op2;
+    double op2, op1;
     char string[MAX_OPERATION_SIZE];
 
     while ((type = getop(string)) != EOF)
@@ -34,6 +38,9 @@ int main()
         {
             case NUMBER:
                 push(atof(string));
+                break;
+            case COMPLEX_MATH:
+                push(complexMath(string));
                 break;
             case '+':
                 push(pop() + pop());
@@ -60,8 +67,26 @@ int main()
                 else
                     printf("error: divided by zero");
                 break;
+            case '?':
+                op1 = pop();
+                push(op1);
+                printf("last element of the stack: %f\n",op1);
+                break;
+            case 'c':
+                clearStack();
+                break;
+            case 'd':
+                op1 = pop();
+                push(op1);
+                push(op1);
+                break;
+            case 's':
+                reverseLastToItemsInStack();
+                break;
+            case 'r':
+                printf("result is: %.8g\n", pop());
+                break;
             case '\n':
-                printf("\t%.8g\n", pop());
                 break;
             default:
                 printf("unknown command %s\n", string);
@@ -85,7 +110,7 @@ double pop()
         return stack[--nextFreeStackPosition];
     else
     {
-        printf("Error: stack empty");
+        printf("Stack is empty\n");
         return 0.0;
     }
 }
@@ -96,10 +121,26 @@ int getop(char string[])
     while ((string[0] = c = getch()) == ' ' || c == '\t')
         ;
 
-    string[1] = '\0';
+
+    i = 1;
 
     if (!isdigit(c) && c != '.' && c != '-')
-        return c;
+    {
+        while (isalpha(string[i++] = c = getch()))
+            ;
+
+        string[--i] = '\0';
+        c = string[i - 1];
+
+        if (strlen(string) == 3)
+            return COMPLEX_MATH;
+        else if(strlen(string) == 1)
+            return c;
+        else
+            printf("Unknown Command: %s\n", string);
+    }
+
+    string[1] = '\0';
 
     i = 0;
 
@@ -113,6 +154,7 @@ int getop(char string[])
             return '-';
         }
     }
+
     if (isdigit(c))
         while (isdigit(string[++i] = c = getch()))
             ;
@@ -131,7 +173,7 @@ int getop(char string[])
 
 int getch(void)
 {
-    return (nextFreePositionInBuffer >= BUFFER_SIZE) ? buffer[--nextFreeStackPosition] : getchar();
+    return (nextFreePositionInBuffer >= BUFFER_SIZE) ? buffer[--nextFreePositionInBuffer] : getchar();
 }
 
 void ungetch(int c)
@@ -140,4 +182,43 @@ void ungetch(int c)
         printf("ungetch: too many characters\n");
     else
         buffer[nextFreePositionInBuffer++] = c;
+}
+
+void reverseLastToItemsInStack()
+{
+    double first = pop();
+    double second = pop();
+    push(first);
+    push(second);
+}
+
+void clearStack()
+{
+    while (pop() != 0.0)
+        ;
+}
+
+double complexMath(char string[])
+{
+    double op1 = pop();
+
+    if (strcmp(string, "sin") == 0)
+        return sin(op1);
+    else if (strcmp(string, "cos") == 0)
+        return cos(op1);
+    else if (strcmp(string, "tan") == 0)
+        return tan(op1);
+    else if (strcmp(string, "cot") == 0)
+        return 1/tan(op1);
+    else if (strcmp(string, "exp") == 0)
+        return exp(op1);
+    else if (strcmp(string, "pow") == 0)
+    {
+        double op2 = pop();
+        return pow(op2,op1);
+    }
+    else
+        printf("Unknown command: %s\n",string);
+
+    return 0.0;
 }
